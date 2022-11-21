974dc64c89bbac074dfee82b654d8315523760a6
Evandro Tozi Moura <112659794+EvandroTM@users.noreply.github.com>
2022-11-21 13:56:13 -0300

    Create aula028.py

---
+++ Aulas/aula028.py
@@ -0,0 +1,34 @@
+'''
+Exercício
+Peça ao usuário para digitar seu nome
+Peça ao usuário par adigitar sua idade
+Se nome e idade forem digitados:
+    Exiba:
+        Seu nome é {nome}
+        Seu nome invertido é {nome invertido}
+        Se nome contém (ou não) espaços
+        A primeira letra do seu nome é {letra}
+        A última letra do seu nome é {letra}
+Se nada for digitado em nome ou idade:
+    Exiba " Desculpe, você deixo campos varios."
+'''
+
+nome = input('Digite seu nome: ')
+idade = input('Digte sua idade: ')
+
+if nome and idade:
+    print(f'Seu nome é {nome}')
+    print(f'Seu nome invertido é {nome[::-1]}')
+    
+    if ' ' in nome:
+        print('Seu nome contém espaços')
+    else:
+        print('Seu nome não contém espaços')
+
+    print(f'Seu nome contém {len(nome)} letras')
+    print(f'A primeira letra do seu nome é {nome[0]}')
+    print(f'A última letra do seu nome é {nome[-1]}')
+else:
+    print(" Desculpe, você deixo campos varios.")
+
+
