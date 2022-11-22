d2f28d2a41da6f48f315ebe4621bfb49e31db354
Evandro Tozi Moura <112659794+EvandroTM@users.noreply.github.com>
2022-11-21 21:29:38 -0300

    ex 37 e 38

--- Aulas/aula037.py
+++ Aulas/aula037.py
@@ -5,13 +5,23 @@ executa uma ação enquanto uma condição for verdadeira
 Loop inifinito → quando um código não tem fim.
 '''
 
-contador = 0
+# contador = 0
 
-while contador <= 10:
-    contador += 1
-    print(contador)
+# while contador <= 100:
+#     contador += 1
 
-    if contador == 4:
-        break
+#     if contador == 6:
+#         print('Pulei o 6')
+#         continue
+
+#     if contador >= 10 and contador <= 27:
+#         print('Pulei o', contador)
+#         continue
+
+#     print(contador)
+
+#     if contador == 40:
+#         break
     
-print('Fim')   
+# print('Fim')   
+
