9fc5e7e9528ae2dbe631746aa6e2ebd9d9746905
Evandro Tozi Moura <112659794+EvandroTM@users.noreply.github.com>
2022-12-02 20:12:40 -0300

    aula ate 60

---
+++ Aulas/aula059.py
@@ -0,0 +1,31 @@
+'''
+DEsempacotamento em chamadas
+de métodos e funções
+'''
+string = 'ABCD'
+lista = ['Maria', 'Helena', 1, 2, 3, 'Eduarda']
+tupla = 'Python', 'é', 'legal'
+salas = [
+    #   0        1
+    ['Maria', 'Helena'], # 0
+    #    0
+    ['Elaine'],  # 1
+    #  0        1        2
+    ['Luiz', 'João', 'Eduarda', (0, 10, 20, 30, 40)], # 2
+]
+
+# p, b, *_, u = lista
+# print(p, u)
+
+# for nome in lista:
+#     print(nome, end=' ')
+
+# print('Maria', 'Helena', 1, 2, 3, 'Eduarda')
+# print(*lista)
+# print(*string)
+# print(*tupla)
+
+print(salas)
+print(*salas)
+print(*salas, end='\n')
+print(*salas, sep='\n')
