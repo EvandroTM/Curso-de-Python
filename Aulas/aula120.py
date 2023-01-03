4da2f04b3d519fd7e1a6a563fabc2dad64f5580c
Evandro Tozi Moura <112659794+EvandroTM@users.noreply.github.com>
2023-01-03 20:45:48 -0300

    aula 120

---
+++ Aulas/aula120.py
@@ -0,0 +1,29 @@
+# Positional-Only Parameters (/) e Keyword-Only Arguments (*)
+# *args (ilimitado de argumentos posicionais)
+# **kwargs (ilimitado de argumentos nomeados)
+# 🟢 Positional-only Parameters (/) - Tudo antes da barra deve
+# ser ❗️APENAS❗️ posicional.
+# PEP 570 – Python Positional-Only Parameters
+# https://peps.python.org/pep-0570/
+# 🟢 Keyword-Only Arguments (*) - * sozinho ❗️NÃO SUGA❗️ valores.
+# PEP 3102 – Keyword-Only Arguments
+# https://peps.python.org/pep-3102/
+
+# def soma (*args):
+#     print(sum(args))
+
+# soma(1)
+# soma(1, 2)
+
+# def soma (a, b, /, x, y):
+#     print(a + b + x + y)
+
+# # soma(1, 2, 3)
+# soma(1, 2, 3, y=3)
+# # soma(x=1, y=2)
+
+def soma(a, b, *args):
+    print(args)
+    print(a + b)
+
+# soma(1, 2, 'qualquer coisa')
