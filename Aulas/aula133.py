cc2f5d52549e0c804fe8aa0fcbfcca2110cdc352
Evandro Tozi Moura <112659794+EvandroTM@users.noreply.github.com>
2023-01-07 00:26:12 -0300

    133

--- Aulas/aula133.py
+++ Aulas/aula133.py
@@ -15,4 +15,15 @@
 
 class Foo:
     def __init__(self):
-        ...
+        self.public = 'Isso é público.'
+        self._protected = 'Isso é protegido.'
+
+
+    def metodo_publico(self):
+        return 'metodo_publico.'
+
+        
+f = Foo()
+print(f._protected)
+# print(f.public)
+# print(f.metodo_publico)
