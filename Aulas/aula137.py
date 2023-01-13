21a11baf98a2e8ad20afc90f768c2410f76ec8c1
Evandro Tozi Moura <112659794+EvandroTM@users.noreply.github.com>
2023-01-13 15:06:42 -0300

    137

---
+++ Aulas/aula137.py
@@ -0,0 +1,14 @@
+# Exercício com classes
+# 1 - Crie uma classe Carro (Nome)
+# 2 - Crie uma classe Motor (Nome)
+# 3 - Crie uma classe Fabricante (Nome)
+# 4 - Faça a ligação entre Carro tem um Motor
+# Obs.: Um motor pode ser de vários carros
+# 5 - Faça a ligação entre Carro e um Fabricante
+# Obs.: Um fabricante pode fabricar vários carros
+# Exiba o nome do carro, motor e fabricante na tela
+class Carro:
+    def __init__(self, nome):
+        self.nome = nome
+        self._motor = None
+        self._fabricante = None
