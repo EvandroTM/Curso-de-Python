0372ebaf0321879c3218cb79c54b2ef385f78985
Evandro Tozi Moura <112659794+EvandroTM@users.noreply.github.com>
2023-01-14 16:38:20 -0300

    138

--- Aulas/aula137.py
+++ Aulas/aula137.py
@@ -12,3 +12,54 @@ class Carro:
         self.nome = nome
         self._motor = None
         self._fabricante = None
+
+    @property
+    def motor(self):
+        return self._motor
+
+    @motor.setter
+    def motor(self, valor):
+        self._motor = valor
+        
+    @property
+    def fabricante(self):
+        return self._fabricante
+
+    @fabricante.setter
+    def fabricante(self, valor):
+        self._fabricante = valor
+
+class Motor:
+    def __init__(self, nome):
+        self.nome = nome
+
+class Fabricante:
+    def __init__(self, nome):
+        self.nome = nome
+
+fusca = Carro('Fusca')
+volkswagen = Fabricante('Volkswagen')
+motor_1_0 = Motor('1.0')
+fusca.fabricante = volkswagen
+fusca.motor = motor_1_0
+print(fusca.nome, fusca.fabricante.nome, fusca.motor.nome)
+
+gol = Carro('Gol')
+volkswagen = Fabricante('Volkswagen')
+motor_1_0 = Motor('1.0')
+gol.fabricante = volkswagen
+gol.motor = motor_1_0
+print(gol.nome, gol.fabricante.nome, gol.motor.nome)
+
+uno = Carro('Uno')
+fiat = Fabricante('Fiat')
+uno.fabricante = fiat
+uno.motor = motor_1_0
+print(uno.nome, uno.fabricante.nome, uno.motor.nome)
+
+fusion = Carro('Fusion')
+ford = Fabricante('Ford')
+motor_2_0 = Motor('2.0 Turbo')
+fusion.fabricante = ford
+fusion.motor = motor_2_0
+print(fusion.nome, fusion.fabricante.nome, fusion.motor.nome)
