88d0031d3efaacfa170376d47d23767717fb6cbb
Evandro Tozi Moura <112659794+EvandroTM@users.noreply.github.com>
2023-01-23 19:33:28 -0300

    155

--- Aulas/aula154.py
+++ Aulas/aula154.py
@@ -3,7 +3,13 @@
 class Multiplicar:
     def __init__(self, multiplicador):
         self._multiplicador = multiplicador
-        
+          
+    def __call__(self, func):
+        def interna(*args, **kwargs):
+            resultado = func(*args, **kwargs)
+            return resultado * self._multiplicador
+        return interna
+
     # def __init__(self, func):
     #     self.func = func
     #     self._multiplicador = 10
@@ -11,14 +17,9 @@ class Multiplicar:
     # def __call__(self, *args, **kwargs):
     #     resultado = self.func(*args, **kwargs)
     #     return resultado * self._multiplicador
-    
-    def __call__(self, func):
-        def interna(*args, **kwargs):
-            resultado = func(*args, **kwargs)
-            return resultado * self._multiplicador
-        return interna
 
-@Multiplicar
+
+@Multiplicar(2)
 def soma(x, y):
     return x + y
 
